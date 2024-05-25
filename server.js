40ce8180125acc7e8d2608833da35d2341366b13
Juantrevi <juantrevi70@gmail.com>
2024-05-25 18:30:41 +0200

    -- Basic CRUD for jobs --

--- server.js
+++ server.js
@@ -66,7 +66,7 @@ app.get('/api/v1/jobs', (req, res) => {
 app.post('/api/v1/jobs', (req, res) => {
     const { company, position } = req.body;
     if (!company || !position) {
-        return res.status(400).json({ message: 'Please provide company and position'});
+        return res.status(400).json({ msg: 'Please provide company and position'});
     }
     const id = nanoid(10);
     const job = { id, company, position };
@@ -79,11 +79,40 @@ app.get('/api/v1/jobs/:id', (req, res) => {
     const { id } = req.params;
     const job = jobs.find(job => job.id === id);
     if (!job) {
-        return res.status(404).json({ message: `Job not found with id: ${id}`});
+        return res.status(404).json({ msg: `Job not found with id: ${id}`});
     }
     res.status(200).json({ job });
 });
 
+// EDIT JOB
+app.patch('/api/v1/jobs/:id', (req, res) => {
+    const { company, position } = req.body;
+    if (!company || !position) {
+        return res.status(400).json({ msg: 'please provide company and position' });
+    }
+    const { id } = req.params;
+    const job = jobs.find((job) => job.id === id);
+    if (!job) {
+        return res.status(404).json({ msg: `no job with id ${id}` });
+    }
+
+    job.company = company;
+    job.position = position;
+    res.status(200).json({ msg: 'job modified', job });
+});
+
+// DELETE JOB
+app.delete('/api/v1/jobs/:id', (req, res) => {
+    const { id } = req.params;
+    const job = jobs.find((job) => job.id === id);
+    if (!job) {
+        return res.status(404).json({ msg: `no job with id ${id}` });
+    }
+    const newJobs = jobs.filter((job) => job.id !== id);
+    jobs = newJobs;
+
+    res.status(200).json({ msg: 'job deleted' });
+});
 
 const port = process.env.PORT || 5100;
 app.listen(port, () => {
