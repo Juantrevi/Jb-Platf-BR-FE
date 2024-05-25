76ded13a29b4da977699e28da6172858a6c2bb23
Juantrevi <juantrevi70@gmail.com>
2024-05-25 16:41:22 +0200

    -- Fetch API, Top level await, watch mode configured --

--- server.js
+++ server.js
@@ -14,6 +14,30 @@ if (process.env.NODE_ENV === 'development') {
 
 app.use(express.json());
 
+/*
+* Example: Fetch API
+* This is a way of fetching an API from a server
+* No need to use anything extra like axios
+* */
+// fetch('https://www.course-api.com/react-useReducer-cart-project')
+//     .then(res => res.json())
+//     .then(data => console.log(data));
+
+/*
+* Example: Top level AWAIT in Node.js
+* We can right away use await in the top level of the file
+* whenever we need (Feature used when connecting to a DB)
+* */
+// try {
+//     const data = await fetch('https://www.course-api.com/react-useReducer-cart-project');
+//     const dataJson = await data.json();
+//     console.log(dataJson);
+// }catch (e) {
+//     console.log(e);
+// }
+
+
+
 app.get('/', (req, res) => {
     res.send('Hello world')
 });
