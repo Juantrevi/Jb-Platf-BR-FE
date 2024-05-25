136d11d0e928f6363bbcbd25f526a43d09f61033
Juantrevi <juantrevi70@gmail.com>
2024-05-25 18:22:03 +0200

    -- GET and POST for jobs --

--- server.js
+++ server.js
@@ -57,8 +57,31 @@ app.post('/', (req, res) => {
     res.json({ message: 'Data received', data: req.body});
 });
 
+//GET ALL JOBS
 app.get('/api/v1/jobs', (req, res) => {
     res.status(200).json({ jobs });
+});
+
+//CREATE A JOB
+app.post('/api/v1/jobs', (req, res) => {
+    const { company, position } = req.body;
+    if (!company || !position) {
+        return res.status(400).json({ message: 'Please provide company and position'});
+    }
+    const id = nanoid(10);
+    const job = { id, company, position };
+    jobs.push(job);
+    res.status(201).json({ job });
+});
+
+//GET SINGLE JOB
+app.get('/api/v1/jobs/:id', (req, res) => {
+    const { id } = req.params;
+    const job = jobs.find(job => job.id === id);
+    if (!job) {
+        return res.status(404).json({ message: `Job not found with id: ${id}`});
+    }
+    res.status(200).json({ job });
 });
 
 
