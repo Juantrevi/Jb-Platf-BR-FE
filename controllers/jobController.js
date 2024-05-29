5432fa44693eb96125dcaeff45733f3468cb0fd9
Juantrevi <juantrevi70@gmail.com>
2024-05-29 23:14:05 +0200

    -- Job Creation on mongo --

--- controllers/jobController.js
+++ controllers/jobController.js
@@ -1,4 +1,5 @@
 import { nanoid } from 'nanoid';
+import Job from '../models/jobModel.js';
 
 let jobs = [
     { id: nanoid(), company: 'apple', position: 'front-end' },
@@ -12,7 +13,8 @@ export const getAllJobs = async (req, res) => {
     res.status(200).json({ jobs });
 }
 
-export const createJob = async (req, res) => {
+/* Create a new job (Local Data)
+*export const createJob = async (req, res) => {
     const { company, position } = req.body;
     if (!company || !position) {
         return res.status(400).json({ msg: 'Please provide company and position'});
@@ -22,6 +24,15 @@ export const createJob = async (req, res) => {
     jobs.push(job);
     res.status(201).json({ job });
 }
+*/
+
+// Create a new job (MongoDB)
+export const createJob = async (req, res) => {
+    const { company, position } = req.body;
+    const job = await Job.create({ company, position });
+    res.status(201).json({ job });
+};
+
 
 export const getSingleJob = async (req, res) => {
     const { id } = req.params;
