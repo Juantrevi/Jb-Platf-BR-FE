b752c531596987f070570e3984e0a41f75243bd1
Juantrevi <juantrevi70@gmail.com>
2024-05-31 15:36:20 +0200

    -- Hashing password in passwordUtils.js --

--- controllers/authController.js
+++ controllers/authController.js
@@ -1,22 +1,20 @@
 import {StatusCodes} from "http-status-codes";
 import  User  from "../models/userModel.js";
-import bcrypt from 'bcryptjs';
+import { hashPassword } from "../utils/passwordUtils.js";
+
 
 export const register = async (req, res) => {
     // If the amount of users is 0, the first user will be an admin
     const isFirstAccount = await User.countDocuments() === 0;
     req.body.role = isFirstAccount ? 'admin' : 'user';
 
-    // Hash the password
-    const salt = await bcrypt.genSalt(10);
-    const hashedPassword = await bcrypt.hash(req.body.password, salt);
+    // Hash the password wit passwordUtils.js
+    const hashedPassword = await hashPassword(req.body.password);
     req.body.password = hashedPassword;
 
     // Create a new user
     const user = await User.create(req.body);
     res.status(StatusCodes.CREATED).json({ msg: 'User created' });
-
-    res.send('register');
 };
 
 export const login = async (req, res) => {
