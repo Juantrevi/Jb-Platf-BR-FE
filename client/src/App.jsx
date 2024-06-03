4c6f946436404d4fac8d304c94d0f8ac4fb792ff
Juantrevi <juantrevi70@gmail.com>
2024-06-03 17:08:01 +0200

    -- Add job functionality --

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -14,6 +14,7 @@ import {
 } from "./pages/index.js";
 import { action as registerAction} from "./pages/Register.jsx";
 import { action as loginAction} from "./pages/Login.jsx";
+import { action as addJob} from "./pages/AddJob.jsx";
 import {loader as dashboardLoader} from "./pages/DashboardLayout.jsx";
 
 
@@ -58,6 +59,7 @@ const router = createBrowserRouter([
                     {
                         index: true,
                         element: <AddJob/>,
+                        action: addJob,
                     },
                     {
                         path: "stats",
--- client/src/pages/AddJob.jsx
+++ client/src/pages/AddJob.jsx
@@ -7,10 +7,26 @@ import { toast } from 'react-toastify';
 import customFetch from '../utils/customFetch';
 
 
+export const action = async ({ request }) => {
+    const formData = await request.formData();
+    const data = Object.fromEntries(formData);
+
+    console.log(data)
+    try {
+        await customFetch.post('/jobs', data);
+        toast.success('Job added successfully');
+        return redirect('all-jobs');
+    } catch (error) {
+        toast.error(error?.response?.data?.msg);
+        return error;
+    }
+};
+
 const AddJob = () => {
     const { user } = useOutletContext();
     const navigation = useNavigation();
     const isSubmitting = navigation.state === 'submitting';
+
 
     return (
         <Wrapper>
@@ -26,14 +42,14 @@ const AddJob = () => {
                         defaultValue={user.location}
                     />
                     <FormRowSelect
-                        name='job status'
-                        labelText='jobStatus'
+                        name='jobStatus'
+                        labelText='Job Status'
                         defaultValue={JOB_STATUS.PENDING}
                         list={Object.values(JOB_STATUS)}
                     />
                     <FormRowSelect
-                        name='job type'
-                        labelText='jobType'
+                        name='jobType'
+                        labelText='Job Type'
                         defaultValue={JOB_TYPE.FULL_TIME}
                         list={Object.values(JOB_TYPE)}
                     />
