63e10f6e36a0da0805a8c15f0aff6493706b093b
Juantrevi <juantrevi70@gmail.com>
2024-06-07 14:20:48 +0200

    -- OnChange functionality on the SearchContainer.jsx, FormRow.jsx and SearchContainer.jsx --

--- client/src/components/SearchContainer.jsx
+++ client/src/components/SearchContainer.jsx
@@ -5,25 +5,27 @@ import {JOB_TYPE, JOB_STATUS, JOB_SORT_BY} from '../../../utils/constants';
 import {useAllJobsContext} from '../pages/AllJobs';
 
 const SearchContainer = () => {
+    const { searchValues } = useAllJobsContext();
+    const { search, jobStatus, jobType, sort } = searchValues;
     const submit = useSubmit();
     return (
         <Wrapper>
             <Form className='form'>
                 <h5 className='form-title'>Search Form</h5>
                 <div className='form-center'>
-                    <FormRow type='search' name='search' defaultValue='a' onChange={(e) => {
+                    <FormRow type='search' name='search' defaultValue={search} onChange={(e) => {
                         submit(e.currentTarget.form)
                     }}/>
                     <FormRowSelect labelText='job status' name='jobStatus' list={['all', ...Object.values(JOB_STATUS)]}
-                                   defaultValue='all' onChange={(e) => {
+                                   defaultValue={jobStatus} onChange={(e) => {
                         submit(e.currentTarget.form)
                     }}/>
                     <FormRowSelect labelText='job type' name='jobType' list={['all', ...Object.values(JOB_TYPE)]}
-                                   defaultValue='all' onChange={(e) => {
+                                   defaultValue={jobType} onChange={(e) => {
                         submit(e.currentTarget.form)
                     }}/>
                     <FormRowSelect labelText='sort by' name='sort' list={Object.values(JOB_SORT_BY)}
-                                   defaultValue='newest' onChange={(e) => {
+                                   defaultValue={sort} onChange={(e) => {
                         submit(e.currentTarget.form)
                     }}/>
 
--- client/src/pages/AllJobs.jsx
+++ client/src/pages/AllJobs.jsx
@@ -3,44 +3,37 @@ import { JobsContainer, SearchContainer } from '../components';
 import customFetch from '../utils/customFetch';
 import { useLoaderData } from 'react-router-dom';
 import { useContext, createContext } from 'react';
-
-export const loader = async ({request}) => {
-
-    // Get the query parameters from the URL, and convert them to an object
-    const params = Object.fromEntries([
-        ...new URL(request.url).searchParams.entries(),
-    ]);
-
+const AllJobsContext = createContext();
+export const loader = async ({ request }) => {
     try {
-        const data = await customFetch.get('/jobs', {
+        const params = Object.fromEntries([
+            ...new URL(request.url).searchParams.entries(),
+        ]);
+
+        const { data } = await customFetch.get('/jobs', {
             params,
         });
-        console.log('Data from /jobs:', data);
-        return  data ;
+
+        return {
+            data,
+            searchValues: { ...params },
+        };
     } catch (error) {
-        console.error('Error fetching jobs:', error);
-        toast.error(error?.response?.data?.msg);
+        toast.error(error.response.data.msg);
         return error;
     }
-}
+};
 
-// Create a context to pass the data to
-const AllJobsContext = createContext();
 const AllJobs = () => {
-    const { data } = useLoaderData();
-    // Pass the data to the context provider. It will be available to all children
-    // like passing props to all children, but with context. This is a better way to
-    // pass data to deeply nested components.
+    const { data, searchValues } = useLoaderData();
+
     return (
-        <AllJobsContext.Provider value={{ data }}>
+        <AllJobsContext.Provider value={{ data, searchValues }}>
             <SearchContainer />
             <JobsContainer />
         </AllJobsContext.Provider>
     );
 };
+export default AllJobs;
 
-// Create a custom hook to use the data in the context
-// This is a better way to access the data in the context
 export const useAllJobsContext = () => useContext(AllJobsContext);
-
-export default AllJobs
