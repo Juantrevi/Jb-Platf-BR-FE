82461438dde46337e7beb5bbfe8acba650a1ba59
Juantrevi <juantrevi70@gmail.com>
2024-06-07 14:37:54 +0200

    -- Complex Pagination --

--- client/src/components/JobsContainer.jsx
+++ client/src/components/JobsContainer.jsx
@@ -1,15 +1,11 @@
-// Import necessary components and hooks
 import Job from './Job';
 import Wrapper from '../assets/wrappers/JobsContainer';
+import PageBtnContainer from './PageBtnContainer';
 import { useAllJobsContext } from '../pages/AllJobs';
 
-// Define the JobsContainer component
 const JobsContainer = () => {
-    // Use the useAllJobsContext hook to get the data
     const { data } = useAllJobsContext();
-    const { jobs } = data;
-
-    // If there are no jobs, display a message
+    const { jobs, totalJobs, numOfPages } = data;
     if (jobs.length === 0) {
         return (
             <Wrapper>
@@ -18,18 +14,19 @@ const JobsContainer = () => {
         );
     }
 
-    // Otherwise, render the jobs
     return (
         <Wrapper>
+            <h5>
+                {totalJobs} job{jobs.length > 1 && 's'} found
+            </h5>
             <div className='jobs'>
-                {/* Map over the jobs array and render a Job component for each job */}
                 {jobs.map((job) => {
                     return <Job key={job._id} {...job} />;
                 })}
             </div>
+            {numOfPages > 1 && <PageBtnContainer />}
         </Wrapper>
     );
 };
 
-// Export the JobsContainer component
 export default JobsContainer;
